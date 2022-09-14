ac9ed3a3bcad1efbcdb370a2f12a4bd6df956a1d
Tim Hinz <tim@chargeover.com>
2022-09-14 12:01:34 -0500

    feat: testing probes in entrypoint script

--- docker/scripts/docker_entrypoint.sh
+++ docker/scripts/docker_entrypoint.sh
@@ -2,5 +2,9 @@
 
 LOG_LEVEL=INFO
 /usr/sbin/glusterd -p /var/run/glusterd.pid  --log-level $LOG_LEVEL $GLUSTERD_OPTIONS
+sleep 10s
+gluster peer probe gluster-1
+gluster peer probe gluster-2
+gluster peer probe gluster-3
 
 exec "$@"
